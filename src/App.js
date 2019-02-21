c84e7d1852c931bcf72b1a5c7f140cbc22d218f6
Raja Simon <rajasimon@icloud.com>
2019-02-21 11:25:02 +0530

    Update codebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+
+  constructor() {
+    super()
+    
+    this.state = {
+      changeInterval: 2000,
+      changeWhite: 'lightWhite',
+      changeRed: 'lightWhite',
+      changeYellow: 'lightWhite',
+      changeGreen: 'lightWhite'
+    }
+
+    this.handleStart = this.handleStart.bind(this)
+    this.handleStop =  this.handleStop.bind(this)
+  }
+
+  handleStart() {
+    this.setState({changeRed: 'lightRed'})
+
+    setInterval(() => {
+      this.setState({changeYellow: 'lightYellow'})
+      this.setState({changeRed: 'lightWhite'})
+
+        setInterval(() => {
+          this.setState({changeGreen: 'lightGreen'})
+          this.setState({changeRed: 'lightWhite'})
+          this.setState({changeYellow: 'lightWhite'})
+        }, this.state.changeInterval);
+
+    }, this.state.changeInterval);
+
+  }
+
+  handleStop() {
+    this.setState({
+      changeRed: 'lightWhite',
+      changeYellow: 'lightWhite',
+      changeGreen: 'lightWhite'
+    })
+
+    clearInterval(this.state.changeInterval)
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={this.state.changeRed + " lightStyle"}></div>
+        <div className={this.state.changeYellow + " lightStyle"}></div>
+        <div className={this.state.changeGreen + " lightStyle"}></div>
+
+
+        <button onClick={this.handleStart}>Start</button>
+        <button onClick={this.handleStop}>Stop</button>
       </div>
     );
   }
